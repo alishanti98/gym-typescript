8148c19599d81f148bb2c4b647460b51587b7837
alishanti98 <alialshanti90@gmail.com>
2023-03-05 13:13:50 +0200

    create project gym-typescript

---
+++ src/assets/index.ts
@@ -0,0 +1,41 @@
+import AbstractWaves from "./AbstractWaves.png";
+import Arrow from "./Arrow.png";
+import BenefitsPageGraphic from "./BenefitsPageGraphic.png";
+import Circles from "./Circles.png";
+import ContactUsPageGraphic from "./ContactUsPageGraphic.png";
+import EvolveText from "./EvolveText.png";
+import HomePageGraphic from "./HomePageGraphic.png";
+import HomePageText from "./HomePageText.png";
+import Logo from "./Logo.png";
+import Sparkles from "./Sparkles.png";
+import SponsorForbes from "./SponsorForbes.png";
+import SponsorFortune from "./SponsorFortune.png";
+import SponsorRedBull from "./SponsorRedBull.png";
+import image1 from "./image1.png";
+import image2 from "./image2.png";
+import image3 from "./image3.png";
+import image4 from "./image4.png";
+import image5 from "./image5.png";
+import image6 from "./image6.png";
+
+export {
+  ContactUsPageGraphic,
+  EvolveText,
+  HomePageGraphic,
+  AbstractWaves,
+  HomePageText,
+  image1,
+  image2,
+  image3,
+  Logo,
+  image4,
+  image5,
+  image6,
+  Arrow,
+  BenefitsPageGraphic,
+  Circles,
+  SponsorRedBull,
+  SponsorFortune,
+  SponsorForbes,
+  Sparkles,
+};
